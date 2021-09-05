23b82010682ecc4e49ef3e4f418b9a3b52fb5aca
Yukiya05 <te200368@cis.fukuoka-u.ac.jp>
2021-09-05 16:42:46 +0900

    kouki

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper('.sample01 .swiper-container', {
-      pagination: '.swiper-pagination',
-      paginationClickable: true,
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      loop: true
-    });
-  }, false);
+$(document).ready(function(){
+	//
+	// 手順3 関数アプリのURLへアクセス
+	$.get( "https://kansuudayoooooo.azurewebsites.net/api/kaunta?code=rntuwEpnPsSG4l5hxdAi1jblxGRbskkxkDuZ86tFUS/MsafcV0gmqA==" ).done( function(value) {
+		//
+		// 手順９ 表示する
+		$("#text").html(value);
+	});
+});
